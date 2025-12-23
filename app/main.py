b92dae6d86134c3c604a386d525ec09753fefb3c
Alkhakim828 <alhakim.7409@gmail.com>
2025-12-23 19:55:08 +0500

    Add project files\

--- app/main.py
+++ app/main.py
@@ -10,6 +10,8 @@ from app.auth.auth import register_user, login_user
 from app.messaging.messaging import send_message, read_messages, init_user_keys
 from app.files.files import encrypt_file, decrypt_file
 
+from app.blockchain.blockchain import Blockchain
+
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
 
@@ -32,6 +34,7 @@ def register(username: str = Form(...), password: str = Form(...)):
         <p>TOTP Secret (add to Authenticator): {secret}</p>
         <a href="/">Back</a>
     """)
+
 
 
 @app.post("/login")
@@ -126,109 +129,14 @@ async def decrypt_file_web(request: Request, file: UploadFile = File(...)):
         media_type="application/octet-stream"
     )
 
-
-
-
-
-
-
-
-
-
-
-# from fastapi import FastAPI, Request, Form
-# from fastapi.responses import HTMLResponse, RedirectResponse
-# from fastapi.templating import Jinja2Templates
-
-# from app.auth.auth import register_user, login_user
-# from app.messaging.messaging import send_message, read_messages, init_user_keys
-# from app.files.files import encrypt_file
-
-# from fastapi import FastAPI, Request, UploadFile, File, Form
-# from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
-# import shutil
-# import os
-
-# from app.files.files import encrypt_file, decrypt_file
-
-
-# app = FastAPI()
-# templates = Jinja2Templates(directory="app/templates")
-
-
-# @app.get("/", response_class=HTMLResponse)
-# def index(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
-
-
-# @app.post("/register")
-# def register(username: str = Form(...), password: str = Form(...)):
-#     secret = register_user(username, password)
-#     init_user_keys(username)
-#     return HTMLResponse(f"""
-#         <h2>Registered</h2>
-#         <p>TOTP Secret (add to Authenticator): {secret}</p>
-#         <a href="/">Back</a>
-#     """)
-
-
-# @app.post("/login")
-# def login(
-#     username: str = Form(...),
-#     password: str = Form(...),
-#     totp: str = Form(...)
-# ):
-#     if login_user(username, password, totp):
-#         return RedirectResponse(
-#             url=f"/dashboard?user={username}",
-#             status_code=302
-#         )
-#     return HTMLResponse("<h2>Login failed</h2><a href='/'>Back</a>")
-
-
-# @app.get("/dashboard", response_class=HTMLResponse)
-# def dashboard(request: Request, user: str):
-#     messages = read_messages(user)
-#     return templates.TemplateResponse(
-#         "dashboard.html",
-#         {
-#             "request": request,
-#             "user": user,
-#             "messages": messages
-#         }
-#     )
-
-
-# @app.post("/send")
-# def send(
-#     sender: str = Form(...),
-#     receiver: str = Form(...),
-#     message: str = Form(...)
-# ):
-#     send_message(sender, receiver, message)
-#     return RedirectResponse(
-#         url=f"/dashboard?user={sender}",
-#         status_code=302
-#     )
-
-# @app.post("/encrypt-file")
-# async def encrypt_file_web(
-#     request: Request,
-#     file: UploadFile = File(...),
-# ):
-#     username = request.cookies.get("user")
-#     if not username:
-#         return RedirectResponse("/login", status_code=302)
-
-#     input_path = f"app/temp/{file.filename}"
-#     with open(input_path, "wb") as buffer:
-#         shutil.copyfileobj(file.file, buffer)
-
-#     encrypted_path = encrypt_file(username, input_path)
-
-#     return FileResponse(
-#         encrypted_path,
-#         filename=os.path.basename(encrypted_path),
-#         media_type="application/octet-stream"
-#     )
-
+@app.get("/audit", response_class=HTMLResponse)
+def audit(request: Request):
+    bc = Blockchain()
+    return templates.TemplateResponse(
+        "audit.html",
+        {
+            "request": request,
+            "chain": bc.chain,
+            "valid": bc.is_chain_valid()
+        }
+    )
